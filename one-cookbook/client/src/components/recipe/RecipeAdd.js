76faee66dfa5cc00d6db64c40674d32e1a14ffa9
Melisa Lee <lee.melisa@gmail.com>
2020-05-20 18:46:14 -0700

    added recipe add api

--- one-cookbook/client/src/components/recipe/RecipeAdd.js
+++ one-cookbook/client/src/components/recipe/RecipeAdd.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 import './styles/RecipeAdd.scss';
 import headerImg from './styles/coverImg.jpg';
+
 
 const COURSES = ['Breakfast', 'Lunch', 'Dinner', 'Soup', 'Side Dish'];
 const CUISINES = ['American', 'Chinese', 'French', 'Koean', 'Japanese', 'Singaporean'];
@@ -15,6 +17,7 @@ class RecipeAdd extends React.Component {
             course: 'Breakfast',
             cuisine: 'American',
             servingSize: 1,
+            ingredients: '',
             instructions: '',
             notes: '',
         };
@@ -26,34 +29,42 @@ class RecipeAdd extends React.Component {
             ...this.state,
             [e.target.name]: e.target.value
         });
-        console.log(`${e.target.name}: ${e.target.value}`)
+        // console.log(`${e.target.name}: ${e.target.value}`)
     }
 
-    handleOnSubmit = (e) => {
+    handleOnSubmit = e => {
         let data = {
             title: this.state.name,
             description: this.state.description,
             course: this.state.course,
-            seving_size: this.state.servingSize,
+            serving_sizes: this.state.servingSize,
             cuisine: this.state.cuisine,
             notes: this.state.notes
         }
         console.log(data);
 
-        // fetch('/recipe', {
-        //     method: 'POST',
-        //     body: JSON.stringify(data),
-        //     header: {
-        //         'Content-Type': 'application/json',
-        //     }
-        // })
-        //     .then(response => {
-        //         console.log('Sucess: ', response.json());
-        //     })
-        //     .catch(error => {
-        //         console.log('Error: ', error);
-        //     })
+        fetch('http://localhost:9000/recipe', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                console.log('Sucess: ', response.json());
+            })
+            .catch(error => {
+                console.log('Error: ', error);
+            })
         e.preventDefault();
+    }
+
+    handleOnAdd = () => {
+        return(
+            <div>
+                hi
+            </div>
+        );
     }
 
     render() {
@@ -124,6 +135,16 @@ class RecipeAdd extends React.Component {
                         </select>
                     </label>
                     <label>
+                        Ingredients:
+                        <input 
+                            type='text' 
+                            name='ngredients' 
+                            // onChange={ e => this.handleInputChange(e)}
+                            className='input_wrapper'
+                            // required
+                        />                        
+                    </label>
+                    <label>
                         Serving Size:
                         <input 
                             type='number' 
@@ -137,14 +158,23 @@ class RecipeAdd extends React.Component {
                     </label>
                     <label>
                         Instructions:
-                        <input 
-                            type='text' 
-                            name='instructions' 
-                            onChange={ e => this.handleInputChange(e)}
-                            className='input_wrapper'
-                            required
-                        />
+                        <div className='instruction_container'>
+                            1.
+                            <input 
+                                type='text' 
+                                name='instructions' 
+                                onChange={ e => this.handleInputChange(e)}
+                                className='instruction_input_wrapper'
+                                required
+                            />
+                            <AddCircleIcon 
+                                onClick={ this.handleOnAdd() }
+                                color='primary'
+                            />                            
+                        </div>
+
                     </label>
+
                     <label>
                         Notes:
                         <textarea 
